39212e9125624604169afd782f9fc8057492311b
sang mook <tkdanr35680@daum.net>
2024-03-21 14:10:00 +0900

    짝수 판별기

--- EvenOdd.java
+++ EvenOdd.java
@@ -3,19 +3,17 @@ import java.util.Scanner;
 public class EvenOdd {
 	public static void main(String args[]) {
 		Scanner input = new Scanner(System.in);
-		System.out.println("정수를 입력하세요");
-		int a;
-		a = input.nextInt();
+		System.out.println("정수를 입력하세요 : ");
+		int num;
+		num = input.nextInt();
 		
-		if(a%2 == 0) {
-			System.out.println("입력된 정수는 짝수입니다.\n");
-			System.out.println("프로그램이 종료됩니다.");
+		if(num%2 == 0) {
+			System.out.println("입력된 정수는 짝수입니다.");
 		}
 		else {
-			System.out.println("입력된 정수는 홀수입니다.\n");
-			System.out.println("프로그램이 종료됩니다.");
+			System.out.println("입력된 정수는 홀수입니다.");
 		}
-
+		System.out.println("프로그램이 종료되었습니다.");
 	}
 
 }
